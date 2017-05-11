ea0f9b5c0a21f4fc094bc7827705a7c5b2d0b3af
nthustraydog <jauy9131253@gapp.nthu.edu.tw>
2017-05-12 02:56:17 +0800

    upload activity

---
+++ docs/activity/decoration.js
@@ -0,0 +1,56 @@
+$(window).load(function(){
+	/*$('html, body').animate({
+		scrollTop: 0
+	}, 500);*/
+});
+$(document).ready(function(){
+	$(".footer").show();
+	new WOW().init();
+	
+	setArea();
+	$(window).on('resize', function(){
+		setArea();
+	});	
+	
+	//share
+	$('.share_twitter').click(function(){
+		window.open('http://twitter.com/share?text=這個粉專超讚！！%0A&url=https://www.facebook.com/NthuCarelife/',
+		'test', 'left = 100, top = 200, width = 600, height = 400');
+	});
+	$('.share_facebook').click(function(){
+		window.open('https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https://www.facebook.com/NthuCarelife/&redirect_uri=https://developers.facebook.com/tools/explorer',
+		'test', 'left = 100, top = 200, width = 600, height = 600');
+	});
+	$('.share_google').click(function(){
+		window.open('https://plus.google.com/share?url=https%3A//www.facebook.com/NthuCarelife/',
+		'test', 'left = 100, top = 200, width = 600, height = 600');
+	});
+	
+
+});
+
+//when resize or load website
+function setArea(){
+	var width = window.innerWidth;
+	
+	
+	$(".nav-desktop a").css("font-size", 21*width/1100);
+	$(".nav-desktop a span").css("font-size", 20*width/1100);
+	
+	
+	if(320 <= width && width <= 1024)
+	{
+		$('#hamburger').css("display", "block");
+		$('.nav-desktop').css("display", "none");
+		$("#cover-1").append($("#gal-one"));
+	}
+	else
+	{
+		$('#hamburger').css("display", "none");
+		$('.nav-desktop').css("display", "block");
+		$("#descript-1").append($("#gal-one"));
+	}
+	
+	
+}
+
